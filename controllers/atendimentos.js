8cff358a3ebc2b038bf3302786f64e12787f3b63
Marilia Soares <marisoares1202@gmail.com>
2021-01-06 09:35:34 -0400

    finalizando API REST

--- controllers/atendimentos.js
+++ controllers/atendimentos.js
@@ -19,4 +19,16 @@ module.exports = app => {
         const atendimento = req.body
         Atendimento.adiciona(atendimento, res)
     })
+
+    app.patch('/atendimentos/:id', (req, res) => {
+        const id = parseInt(req.params.id)
+        const valores = req.body
+
+        Atendimento.altera(id, valores, res)
+    })
+
+    app.delete('/atendimentos/:id', (req, res) => {
+        const id = parseInt(req.params.id)
+        Atendimento.deleta(id, res)
+    })
 }
--- models/atendimentos.js
+++ models/atendimentos.js
@@ -40,7 +40,7 @@ class Atendimento {
         if (erro) {
           res.status(400).json(erro)
         } else {
-          res.status(201).json(resultados)
+          res.status(201).json(atendimento)
         }
       })
     }
@@ -68,6 +68,33 @@ class Atendimento {
         res.status(400).json(erro)
       } else {
         res.status(200).json(result[0])
+      }
+    })
+  }
+
+  altera(id, valores, res) {
+    if(valores.data) {
+      valores.data = moment(valores.data, 'DD/MM/YYYY').format('YYYY-MM-DD HH:mm:ss')
+    }
+    const sql = 'UPDATE atendimentos SET ? WHERE id = ?'
+
+    conexao.query(sql, [valores, id], (erro, result) => {
+      if(erro) {
+        res.status(400).json(erro)
+      } else {
+        res.status(200).json({...valores, id})
+      }
+    })
+  }
+
+  deleta(id, res) {
+    const sql = 'DELETE FROM atendimentos WHERE id = ?'
+
+    conexao.query(sql, id, (erro, result) => {
+      if(erro) {
+        res.status(400).json(erro)
+      } else {
+        res.status(200).json({id})
       }
     })
   }
